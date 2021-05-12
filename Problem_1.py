d8702d2b0fcf8315437e0492ed0533aa747c6ba2
KolluriMounish <82930611+KolluriMounish@users.noreply.github.com>
2021-05-12 08:54:23 +0530

    Initial Commit

--- Problem_1.py
+++ Problem_1.py
@@ -1,3 +1,6 @@
+#Implement a group_by_owners function that:
+# ·         Accepts a dictionary containing the file owner name for each file name.
+# ·         Returns a dictionary containing a list of file names for each owner name, in any order.
 def group_by_owners(file_owner_dictionary):
     keys = [key for key, value in file_owner_dictionary.items()]
     values = [value for key, value in file_owner_dictionary.items()]
